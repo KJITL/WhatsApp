005cabb6bd5689f3b4e946b3b9ede3817012ac1a
kp <sii2moh@gmail.com>
2023-01-23 22:11:13 +0300

    'update layout'

--- lib/Widgets/contact_list.dart
+++ lib/Widgets/contact_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whastapp/colors.dart';
 
 import 'package:whastapp/info.dart';
 
@@ -8,21 +9,34 @@ class ContactList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      shrinkWrap: true,
       itemCount: info.length,
       itemBuilder: (context, int index) {
-        return Padding(
-          padding: const EdgeInsets.fromLTRB(0, 6, 0, 6),
-          child: ListTile(
-            title: Text(info[index]['name'].toString(),
-                style: const TextStyle(fontSize: 18)),
-            subtitle: Text(info[index]['message'].toString(),
-                style: const TextStyle(fontSize: 15)),
-            leading: CircleAvatar(
-              backgroundImage:
-                  NetworkImage(info[index]['profilePic'].toString()),
+        return InkWell(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 6, 0, 6),
+            child: Column(
+              children: [
+                ListTile(
+                  title: Text(info[index]['name'].toString(),
+                      style: const TextStyle(fontSize: 18)),
+                  subtitle: Text(info[index]['message'].toString(),
+                      style: const TextStyle(fontSize: 15)),
+                  leading: CircleAvatar(
+                    backgroundImage:
+                        NetworkImage(info[index]['profilePic'].toString()),
+                    radius: 30,
+                  ),
+                  trailing: Text(info[index]['time'].toString(),
+                      style: const TextStyle(fontSize: 13, color: Colors.grey)),
+                ),
+                const Divider(
+                  color: dividerColor,
+                  indent: 85,
+                ),
+              ],
             ),
-            trailing: Text(info[index]['time'].toString(),
-                style: const TextStyle(fontSize: 13, color: Colors.grey)),
           ),
         );
       },
